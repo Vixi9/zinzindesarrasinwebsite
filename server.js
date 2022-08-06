eca0054efc1779c58d711a9c5670132c97e47e13
brionnevi <victor.brionne@gmail.com>
2022-08-06 16:48:33 +0200

    Github action update

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const path = require("path");
 
 const dir = "src/environments";
 const file = "environments.ts";
@@ -8,9 +9,10 @@ const content = `${process.env.ENVIRONMENT}`;
 
 fs.access(dir, fs.constants.F_OK, err => {
   if (err) {
+    console.log("Directory does not exists, creating it now", process.cwd());
     fs.mkdir(dir, {recursive: true}, error => {
       if (error) {
-        console.log(error)
+        console.log(error);
         throw error;
       }
     });
@@ -18,7 +20,10 @@ fs.access(dir, fs.constants.F_OK, err => {
   try {
     fs.writeFileSync(dir + '/' + file, content);
     fs.writeFileSync(dir + '/' + prodFile, content);
-    console.log("Created environment successfully");
+    console.log("Created successfully in", process.cwd());
+    if (fs.existsSync(dir + '/' + file)) {
+      console.log("File created", path.resolve(dir + '/' + file));
+    }
   } catch (error) {
     console.log(error);
     process.exit(1);
