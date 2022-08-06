f88799e5aa10a661992ce6b3e29565b2703316f5
brionnevi <victor.brionne@gmail.com>
2022-08-06 19:11:01 +0200

    Contact page update

--- src/app/website/contact/contact.component.ts
+++ src/app/website/contact/contact.component.ts
@@ -1,31 +1,29 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ActivatedRouteSnapshot} from "@angular/router";
+import {Component} from '@angular/core';
+import {Observable} from "rxjs";
+import {GoogleMapsService} from "../service/google-maps.service";
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss', '../website.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
 
-  // options: google.maps.MapOptions = {
-  //   center: {lat: 48.61312753460924, lng: -0.5919556464350556},
-  //   zoom: 7,
-  //   streetViewControl: false,
-  //   fullscreenControl: false,
-  //   mapTypeControlOptions: {mapTypeIds: ['roadmap']},
-  // }
-  // markerOptions: google.maps.MarkerOptions = {draggable: false};
-  // markerPositions: google.maps.LatLngLiteral[] = [{lat: 48.61312753460924, lng: -0.5919556464350556}];
-
-  private route: ActivatedRouteSnapshot;
-
-  constructor(route: ActivatedRoute) {
-    this.route = route.snapshot;
+  mapOptions: google.maps.MapOptions = {
+    center: {lat: 48.61312753460924, lng: -0.5919556464350556},
+    zoom: 7,
+    streetViewControl: false,
+    fullscreenControl: false,
+    mapTypeControl: false
   }
 
-  ngOnInit(): void {
-    console.log(this.route.data);
-  }
+  markerOptions: google.maps.MarkerOptions = {draggable: false};
+  markerPositions: google.maps.LatLngLiteral = {lat: 48.61312753460924, lng: -0.5919556464350556};
 
+
+  public apiLoaded: Observable<boolean>;
+
+  constructor(googleMaps: GoogleMapsService) {
+    this.apiLoaded = googleMaps.isApiLoaded;
+  }
 }
