3eaccfd053a499fac4e7bb2d90d6099492c148ea
Victor Brionne <victor.brionne@gmail.com>
2022-09-07 18:39:02 +0200

    New home layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,11 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-  title = 'zinzindesarrasin';
+  title = 'zinzindesarrasin'
 }
-
-export const scrollEl = document.documentElement;
-export const root = document.documentElement
-
-export let scrollPos = 0;
-
-export function animation() {
-  if (scrollPos !== scrollEl.scrollTop) {
-    scrollPos = scrollEl.scrollTop
-    root.style.setProperty('--scrollPos', scrollPos + 'px')
-  }
-  window.requestAnimationFrame(animation)
-}
-
-window.requestAnimationFrame(animation)
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MaterialModule} from './material/material.module';
-import {HttpClientJsonpModule, HttpClientModule} from '@angular/common/http';
-import {WebsiteModule} from './website/website.module';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
+import { FlexLayoutModule } from '@angular/flex-layout'
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material/material.module'
+import { HttpClientJsonpModule, HttpClientModule } from '@angular/common/http'
+import { WebsiteModule } from './website/website.module'
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
