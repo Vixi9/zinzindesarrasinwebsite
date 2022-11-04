24d444ecc428203add80b654f6a13fdda72f3c6b
brionnevi <victor.brionne@gmail.com>
2022-11-04 21:22:50 +0100

    Created new layout for products page

--- src/app/website/website.module.ts
+++ src/app/website/website.module.ts
@@ -19,6 +19,7 @@ import { MatButtonModule } from "@angular/material/button";
 import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 
 @NgModule({
   declarations: [
@@ -43,6 +44,7 @@ import { MatToolbarModule } from "@angular/material/toolbar";
     MatListModule,
     MatIconModule,
     MatToolbarModule,
+    MatCardModule,
   ],
   exports: [
     FooterComponent,
